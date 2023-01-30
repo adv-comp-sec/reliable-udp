cfc5d40a12e03fa014403beb9f7c55f62f9bbc18
HYEWONLEE\hyewon <katelavande@gmail.com>
2023-01-30 14:57:35 -0500

    add new file A2_W3.cpp

---
+++ A2_W3/A2_W3.cpp
@@ -0,0 +1,11 @@
+// TODO
+// Load and store a file from / to disk
+
+// Send and receive the file
+
+
+// Verify the file’s contents
+
+
+// Other tasks as needed
+// (don’t forget, you will have to test the file validation)
