f2a6996f639036700cd9d0b87b78539bbfd42ecb
HYEWONLEE\hyewon <katelavande@gmail.com>
2023-01-30 15:16:49 -0500

    add readFile()

--- A2_W3/A2_W3.cpp
+++ A2_W3/A2_W3.cpp
@@ -1,5 +1,40 @@
+// Header comments
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string fileContent = "";
+
 // TODO
 // Load and store a file from / to disk
+void readFile(string fileName)
+{
+	ifstream myfile;
+	myfile.open(fileName);
+	// TODO: file validation
+
+	if (myfile.is_open())
+	{
+		string line;
+		while (getline(myfile, line))
+		{
+			fileContent += line + "\n";
+		}
+		myfile.close();
+	}
+	else
+	{
+		cout << "Unable to open file";
+	}
+
+}
+
+void saveFile()
+{
+
+}
 
 // Send and receive the file
 
